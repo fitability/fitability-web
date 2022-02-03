444b49a2de9f6853658cfd5ad1d82ae39b69007b
Justin Yoo <justin.yoojh@gmail.com>
2022-02-03 15:55:18 +0900

    Add test projects

---
+++ test/Fitability.ApiApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fitability.ApiApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
